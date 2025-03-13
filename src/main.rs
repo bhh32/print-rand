b6fcd39e0fb66398ec12cddcbdac1cab2b1a8efa
Bryan Hyland <bryan.hyland32@gmail.com>
2025-03-13 10:09:48 -0700

    Updated rand::random()

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 fn main() {
     // Generate a random number between 1 and 50
-    let rand_num = rand::rng().random_range(1..=50);
+    let rand_num: i32 = rand::random();
     // Print the random number that was generated
     println!("Random number: {rand_num}");
 }
