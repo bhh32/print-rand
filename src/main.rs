4b01f67cf79226874cc16a6f263c190bd3a1f5ef
Bryan Hyland <bryan.hyland32@gmail.com>
2025-03-13 10:48:20 -0700

    Updated Cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 fn main() {
     // Generate a random number between 1 and 50
-    let rand_num: i32 = rand::random();
+    let rand_num: i32 = rand::thread_rng().gen_range(1..=50);
     // Print the random number that was generated
     println!("Random number: {rand_num}");
 }
